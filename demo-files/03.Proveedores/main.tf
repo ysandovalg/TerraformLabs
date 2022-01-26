a59055a2bb14da564b4f70ad64b3d34483b9708a
car <carlos.echc11@gmail.com>
2022-01-25 21:26:36 -0600

    02

---
+++ demo-files/03.Proveedores/main.tf
@@ -0,0 +1,5 @@
+
+resource "local_file" "mascota" {
+  content  = "Mi mascota es un perro"
+  filename = "mascota.txt"
+}
---
+++ demo-files/03.Proveedores/random.tf
@@ -0,0 +1,4 @@
+resource "random_string" "iac_random" {
+  length    = 10
+  min_upper = 5
+}
