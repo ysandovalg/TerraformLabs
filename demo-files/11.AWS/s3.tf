72231192678f44f786ccf0a0a3e0774dfb05fac1
car <carlos.echc11@gmail.com>
2022-02-11 13:14:26 -0600

    12

--- demo-files/11.AWS/s3.tf
+++ demo-files/11.AWS/s3.tf
@@ -1,7 +1,10 @@
 resource "aws_s3_bucket" "demo_bucket" {
   bucket = "terralabs-15923" # el nombre del bucket debe ser unico.
-  acl    = "public-read"
+}
 
+resource "aws_s3_bucket_acl" "remote_state" {
+  bucket = aws_s3_bucket.demo_bucket.id
+  acl    = "public-read"
 }
 
 resource "aws_s3_bucket_object" "demo_file" {
